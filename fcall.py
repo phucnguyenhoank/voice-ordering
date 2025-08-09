2661e3088de3f5d93c8ea0f14016edcad33e0cb9
phucnguyenhoank <phucnguyeho@gmail.com>
2025-08-09 17:12:23 +0700

    simple utils function

--- fcall.py
+++ fcall.py
@@ -1,8 +1,16 @@
 from utils import llama
 
-prompt = """
-    Who is Mark Zugkerbert?
-"""
-response = llama(prompt)
-print(response)
+context = "Phuc and Mui are friends. Mui and Huy are friends."
 
+query = "Are Phuc and Huy friends?"
+
+prompt_1 = f"""Given the context: {context}. 
+Help me answer the query: {query}. 
+Think step by step, and then give me the answer in the form `answer:yes`, or `answer:no`."""
+
+prompt = "I want to eat your fast food for free."
+
+response_1, ctx = llama(prompt)
+
+print(f"[{response_1}]")
+print(f"context length: {len(ctx)}")
--- utils.py
+++ utils.py
@@ -9,31 +9,29 @@ url = f"http://localhost:11434/api/generate"
 
 
 import time
-def llama(prompt, 
-          add_inst=True, 
+def llama(prompt,
           model="llama3.1", 
           temperature=0.0, 
           max_tokens=1024,
-          verbose=False,
           url=url,
           base = 2, # number of seconds to wait
           max_tries=3,
-          stream=False):
-    
-    if add_inst:
-        prompt = f"[INST]{prompt}[/INST]"
-
-    if verbose:
-        print(f"Prompt:\n{prompt}\n")
-        print(f"model: {model}")
+          stream=False,
+          context=None):
 
     data = {
             "model": model,
             "prompt": prompt,
-            "temperature": temperature,
             "max_tokens": max_tokens,
-            "stream": stream
+            "stream": stream,
+            "options": {
+                "temperature": temperature
+            }
         }
+
+    
+    if context is not None:
+        data["context"] = context
 
     # Allow multiple attempts to call the API incase of downtime.
     # Return provided response to user after 3 failed attempts.    
@@ -43,7 +41,20 @@ def llama(prompt,
         try:
             response = requests.post(url, json=data)
             # print(json.dumps(response.json(), indent=2))  # pretty-print whole JSON
-            return response.json()["response"]
+
+            result = response.json()
+
+            # print load time from API (nanoseconds to seconds)
+            if "load_duration" in result:
+                load_time_sec = result["load_duration"] / 1e9
+                print(f"Model load time: {load_time_sec:.3f} seconds")
+            
+            if "total_duration" in result:
+                total_time_sec = result["total_duration"] / 1e9
+                print(f"Total time taken for the request: {total_time_sec:.3f} seconds")
+
+            return result["response"], result.get("context", [])
+        
         except Exception as e:
             if response.status_code != 500:
                 return response.json()
